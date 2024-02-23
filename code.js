bb30cc07a634d21aa013b9a759cb491f25fb6d90
B10M0NT15 <bairdbrayden99@gmail.com>
2024-02-22 22:48:43 -0800

    Started JS,HTML & CSS

---
+++ code.js
@@ -0,0 +1,17 @@
+function calculate() {
+  console.log("Calc me baby!");
+  const sideA = document.getElementById("side-a");
+  console.log(sideA.value);
+
+  console.log("Calc me baby!");
+  const sideB = document.getElementById("side-b");
+  console.log(sideB.value);
+
+  var answer = Math.sqrt(sideA.value ** 2 + sideB.value ** 2);
+  if (true) {
+    let test = "Me";
+  }
+  answer = "Hi";
+  console.log(test);
+}
+console.log(answer);
