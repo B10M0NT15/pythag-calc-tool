17a0f5d24067c695b6b49d6c49583916eb600fd5
B10M0NT15 <bairdbrayden99@gmail.com>
2024-02-27 20:09:49 -0800

    Input values are now displayed on each side of triangle. Also handled some errors.

--- code.js
+++ code.js
@@ -12,6 +12,28 @@ function highlightSide(side) {
   sideObj[side].style.backgroundColor = "red";
 }
 
+function lblInputs(adjacent, opposite, hypotenuse) {
+  document.getElementById("label-alpha").textContent = adjacent;
+  document.getElementById("label-beta").textContent = opposite;
+  document.getElementById("label-creek").textContent = hypotenuse;
+}
+
+function errorHandler(check1, check2, check3) {
+  const condition1 = check3 <= check1;
+  const condition2 = check3 <= check2;
+  list = [check1, check2, check3];
+  for (let i = 0; i < list.length; i++) {
+    if (isNaN(list[i])) {
+      window.alert("Please make sure your inputs are nummerical!");
+    }
+  }
+  // This if statment below is still popping up even if the for loop above is true.
+  //lets fix this issue.
+  if (condition1 || condition2) {
+    window.alert("Make sure C > A or B!");
+  }
+}
+
 function changeLbls() {
   console.log("Yoooo");
   const sideSolve = document.getElementById("side-selector").value;
@@ -59,9 +81,17 @@ function getValues() {
 
 function solver(inputObj) {
   const { sideToSolve, side1, side2 } = inputObj;
+  errorHandler(sideToSolve, side1, side2);
   if (sideToSolve !== "c") {
     // solve for leg a & b
     const answer = Math.sqrt(side2 ** 2 - side1 ** 2);
+    if (sideToSolve === "a") {
+      lblInputs(answer, side1, side2);
+    } else {
+      lblInputs(side1, answer, side2);
+    }
+    lblInputs(side1, side2, answer);
+
     return answer;
   }
 
